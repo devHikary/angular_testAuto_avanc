e28b46cdf78d868bbbfeb1288a39edaed6f02eff
devHikary <wrkkarina@gmail.com>
2021-09-05 17:09:05 -0300

    Inicialização projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LikeWidgetModule } from './shared/components/like-widget/like-widget.module';
+import { PhotoFrameComponent } from './shared/components/photo-frame/photo-frame.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PhotoFrameComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LikeWidgetModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shared/components/photo-frame/photo-frame.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-frame',
+  templateUrl: './photo-frame.component.html',
+  styleUrls: ['./photo-frame.component.scss']
+})
+export class PhotoFrameComponent implements OnInit {
+
+  @Output() public liked: EventEmitter<void> = new EventEmitter();
+  @Input() public description = '';
+  @Input() public src = '';
+  @Input() public likes = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public like(): void {
+    this.liked.emit();
+  }
+}
