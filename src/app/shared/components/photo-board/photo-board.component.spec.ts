a41bfd8ae91d9ebae0932b8fcd77ccfc58cfba95
devHikary <wrkkarina@gmail.com>
2021-09-06 07:42:28 -0300

    Aula 4.7 Mais surpresas ao testar componentes

--- src/app/shared/components/photo-board/photo-board.component.spec.ts
+++ src/app/shared/components/photo-board/photo-board.component.spec.ts
@@ -1,3 +1,4 @@
+import { Photo } from './interfaces/photo';
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
@@ -5,7 +6,20 @@ import { DebugElement } from '@angular/core';
 
 import { PhotoBoardComponent } from './photo-board.component';
 
-describe('PhotoBoardComponent', () => {
+function buildPhotoList(): Photo[] {
+  const photos: Photo[] = [];
+  for (let i = 0; i < 8; i++) {
+    photos.push({
+      id: i + 1,
+      url: '',
+      description: ''
+    });
+  }
+  return photos;
+}
+
+
+describe(PhotoBoardComponent.name, () => {
   let component: PhotoBoardComponent;
   let fixture: ComponentFixture<PhotoBoardComponent>;
 
@@ -14,15 +28,26 @@ describe('PhotoBoardComponent', () => {
       declarations: [ PhotoBoardComponent ]
     })
     .compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(PhotoBoardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`Should display rows and columns when (@Input photos) has value`, () => {
+    component.photos = buildPhotoList();
+    fixture.detectChanges();
+    expect(component.rows.length)
+      .withContext('Number of rows')
+      .toBe(2);
+    expect(component.rows[0].length)
+      .withContext('Number of columns from the first row')
+      .toBe(4);
+    expect(component.rows[1].length)
+      .withContext('Number of columns from the second row')
+      .toBe(4);
   });
 });
