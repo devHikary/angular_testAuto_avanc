fc5adcfae8a00e6409b5f484258e05395f8eec3b
devHikary <wrkkarina@gmail.com>
2021-09-06 16:25:49 -0300

    Aula 5.7 Spy com resposta programada

--- src/app/components/photo-list/photo-list.component.spec.ts
+++ src/app/components/photo-list/photo-list.component.spec.ts
@@ -1,12 +1,16 @@
+import { PhotoBoardService } from 'src/app/shared/components/photo-board/services/photo-board.service';
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PhotoListComponent } from './photo-list.component';
 import { PhotoListModule } from './photo-list.module';
+import { buildPhotoList } from 'src/app/shared/components/photo-board/test/build-photo-list';
+import { of } from 'rxjs';
 
 describe(PhotoListComponent.name, () => {
   let component: PhotoListComponent;
   let fixture: ComponentFixture<PhotoListComponent>;
+  let service: PhotoBoardService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,9 +22,17 @@ describe(PhotoListComponent.name, () => {
     .compileComponents();
     fixture = TestBed.createComponent(PhotoListComponent);
     component = fixture.componentInstance;
+    service = TestBed.inject(PhotoBoardService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`(D) Should display board when data arrives`, () => {
+    fixture.detectChanges();
+    const photos = buildPhotoList();
+    spyOn(service, 'getPhotos')
+      .and.returnValue(of(photos));
   });
 });
--- src/app/shared/components/photo-board/photo-board.component.spec.ts
+++ src/app/shared/components/photo-board/photo-board.component.spec.ts
@@ -1,23 +1,9 @@
-import { Photo } from './interfaces/photo';
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement, SimpleChange, SimpleChanges } from '@angular/core';
 
 import { PhotoBoardComponent } from './photo-board.component';
-
-function buildPhotoList(): Photo[] {
-  const photos: Photo[] = [];
-  for (let i = 0; i < 8; i++) {
-    photos.push({
-      id: i + 1,
-      url: '',
-      description: ''
-    });
-  }
-  return photos;
-}
-
+import { buildPhotoList } from './test/build-photo-list';
 
 describe(PhotoBoardComponent.name, () => {
   let component: PhotoBoardComponent;
