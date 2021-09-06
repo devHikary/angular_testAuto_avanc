fdd776978bb0c1d552ba5079f46739c87f201ce9
devHikary <wrkkarina@gmail.com>
2021-09-06 15:54:44 -0300

    Aula 5.2 Botando a casa em ordem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { PhotoListModule } from './components/photo-list/photo-list.module';
 import { PhotoBoardModule } from './shared/components/photo-board/photo-board.module';
 import { PhotoFrameModule } from './shared/components/photo-frame/photo-frame.module';
 import { BrowserModule } from '@angular/platform-browser';
@@ -7,18 +8,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LikeWidgetModule } from './shared/components/like-widget/like-widget.module';
 import { HttpClientModule } from '@angular/common/http';
+import { PhotoListComponent } from './components/photo-list/photo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LikeWidgetModule,
-    PhotoFrameModule,
     HttpClientModule,
-    PhotoBoardModule
+    PhotoListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
